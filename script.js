f87f338e2279c6a8847e835aa7fc2aa8d776d170
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2023-07-09 21:39:33 +0300

    +

--- script.js
+++ script.js
@@ -9,3 +9,45 @@ function insert(num) {
     } else
         input.textContent += num;
 }
+
+
+
+//Синусы и косинусы
+function f(name) {
+    if (name == 'sin') {
+        if (document.querySelector('.type').textContent == "deg") {
+            //В градусах
+            input.textContent = parseFloat(Math.sin(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
+        } else {
+            //В радианах
+            input.textContent = parseFloat(Math.sin(eval(input.textContent)).toFixed(8).toString());
+        }
+    }
+    if (name == 'cos') {
+        if (document.querySelector('.type').textContent == "deg") {
+            //В градусах
+            input.textContent = parseFloat(Math.cos(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
+        } else {
+            //В радианах
+            input.textContent = parseFloat(Math.cos(eval(input.textContent)).toFixed(8).toString());
+        }
+    }
+    if (name == 'tan') {
+        if (document.querySelector('.type').textContent == "deg") {
+            //В градусах
+            input.textContent = parseFloat(Math.tan(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
+        } else {
+            //В радианах
+            input.textContent = parseFloat(Math.tan(eval(input.textContent)).toFixed(8).toString());
+        }
+    }
+    if (name == 'ctg') {
+        if (document.querySelector('.type').textContent == "deg") {
+            //В градусах
+            input.textContent = parseFloat(1 / Math.tan(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
+        } else {
+            //В радианах
+            input.textContent = parseFloat(1 / Math.tan(eval(input.textContent)).toFixed(8).toString());
+        }
+    }
+}
